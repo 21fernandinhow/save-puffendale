789c978d4e40e663dcd13f49168ac5744a50d4d6
Fernando Carvalho <21fernandinhow@gmail.com>
2025-12-29 10:10:00 -0300

    ordenatio on task lists by updated_at

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,6 +1,6 @@
 class TaskList < ApplicationRecord
   belongs_to :user
-  has_many :tasks, -> { order(position: :asc) }, dependent: :destroy
+  has_many :tasks, -> { order(updated_at: :desc) }, dependent: :destroy
 
   validates :name, presence: true
 end
