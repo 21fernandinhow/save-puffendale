87280915ad82673cacf728f12df446c83cec3231
Fernando Carvalho <21fernandinhow@gmail.com>
2026-01-09 20:50:34 -0300

    email config devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,26 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  config.action_mailer.default_url_options = {
+    host: "save-puffendale-production.up.railway.app",
+    protocol: "https"
+  }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV.fetch("SMTP_USER"),
+    password: ENV.fetch("SMTP_PASSWORD")
+  }
+
+
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
